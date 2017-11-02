51011fa0110f2b1526b26a788af95aac71d76af5
ChanceYu <i.fish@foxmail.com>
2017-11-02 21:40:15 +0800

    增加url参数解析方法

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # weapp-components
 **微信小程序组件封装（持续更新中...）**
 
-此项目封装一些小程序组件，未使用到第三方框架，您可以很好的使用全部的组件或者单独集成某个组件到自己项目中。另外此项目也对小程序的一些小功能进行了封装，方便开发使用。
+此项目封装一些小程序组件，未使用到第三方框架，您可以很好的使用全部的组件或者单独集成某个组件到自己项目中。另外也对小程序的一些小功能进行了封装，方便开发使用。
+
 
 ### 目录结构
 * `assets` ---------- 静态资源文件
@@ -16,9 +17,27 @@
   * `weapp` ------------- 自定义组件目录
 * `README.md` ------- 项目说明
 
+
+### WeAPP组件
+* Toast浮动提示
+
+
+### 公共方法的封装
+##### common.share 页面转发分享
+除去多页面的繁杂配置，使用起来更加简单高效，支持页面传递参数 options
+```javascript
+  import common from '../../assets/js/common';//注意引入目录
+
+  Page({
+    onShareAppMessage: common.share()
+  });
+```
+
+
 ### 第三方库目前使用到
 * font-awesome (4.7) <https://github.com/FortAwesome/Font-Awesome>
 * weui-wxss <https://github.com/Tencent/weui-wxss>
 
-### WeAPP组件
-* Toast浮动提示
+
+### License
+[![](https://badges.frapsoft.com/os/mit/mit.png?v=103)](https://opensource.org/licenses/mit-license.php) 
--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,51 @@
-/* 公共方法和配置 */
+/* 公共方法封装 */
 
 const common = {
+  /**
+   * 将对象解析成url字符串参数
+   * @param {String} obj 参数对象
+   * @param {Boolean} unEncodeURI 不使用编码
+   */
+  param(obj = {}, unEncodeURI) {
+    let result = [];
+
+    for (let name of Object.keys(obj)) {
+      let value = obj[name];
+
+      result.push(name + '=' + (unEncodeURI ? value : encodeURIComponent(value)));
+    }
+
+    if (result.length) {
+      return '?' + result.join('&');
+    } else {
+      return '';
+    }
+  },
+  /**
+   * 将url字符串参数解析成对象
+   * @param {String} str 带url参数的地址
+   * @param {Boolean} unDecodeURI 不使用解码
+   */
+  unparam(str = '', unDecodeURI) {
+    let result = {};
+    let query = str.split('?')[1];
+
+    if (!query) return result;
+
+    let arr = query.split('&');
+
+    arr.forEach((item, idx) => {
+      let param = item.split('=');
+      let name = param[0];
+      let value = param[1] || '';
+
+      if (name) {
+        result[name] = unDecodeURI ? value : decodeURIComponent(value);
+      }
+    });
+
+    return result;
+  },
   /**
    * 转发分享
    * @param {String} title 标题
@@ -11,10 +56,11 @@ const common = {
       let pages = getCurrentPages();
       let currPage = pages[pages.length - 1];
       let path = '/' + currPage.route;
+      let params = common.param(currPage.options, true);
 
       return {
         title: title,
-        path: url || path
+        path: (url || path) + params
       }
     }
   }
