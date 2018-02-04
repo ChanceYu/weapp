8a08485abe4829f8e63d6c3e3bce7f1d59c9d571
ChanceYu <i.fish@foxmail.com>
2018-02-04 10:20:38 +0800

    修复Tab内容切换展示

--- pages/weapp/tab/tab.js
+++ pages/weapp/tab/tab.js
@@ -1,5 +1,7 @@
 Page({
   data: {
+    activeIndex: 0,
+    activeIndex2: 0,
     list1: ['选项1', '选项2', '选项3'],
     list2: ['活动1', '活动2']
   },
@@ -11,7 +13,14 @@ Page({
       activeIndex: 1
     });
   },
-  onTabChange(event){
-    console.log(event.detail.activeIndex)
+  onTabChange(event) {
+    this.setData({
+      activeIndex: event.detail.activeIndex
+    });
+  },
+  onTabChange2(event) {
+    this.setData({
+      activeIndex2: event.detail.activeIndex
+    });
   }
 })
